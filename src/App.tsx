a113f31710175991362dfec88f2782ae66496dbe
Jadson Santos <jadsondossantos5@gmail.com>
2024-12-18 19:53:16 -0300

    use new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
 import { fetchInfoUser } from './services'
 import { UserInfos } from './types'
-import AddressSection from './sections/Address'
-import PhoneSection from './sections/Phone'
-import EmailSection from './sections/Email'
-import Company from './sections/Company'
 import Person from './sections/Person'
 import SectionWrapper from './components/SectionWrapper'
 import './styles/index.scss'
+import UserDashboard from './sections/UserDashboard'
 
 function App() {
   const [infoUser, setInfoUser] = useState<UserInfos | null>(null)
@@ -26,38 +23,12 @@ function App() {
   }, [])
 
   return (
-    <main>
+    <main className="main">
       {infoUser && (
         <div className="container">
-          <h2>Emails</h2>
-          <SectionWrapper>
-            {infoUser?.email?.map((email) => (
-              <EmailSection key={email['email address']} email={email} />
-            ))}
-          </SectionWrapper>
-          <h2>Endereços</h2>
-          <SectionWrapper>
-            {infoUser.endereco.map((endereco) => (
-              <AddressSection key={endereco.logradouro} endereco={endereco} />
-            ))}
-          </SectionWrapper>
-          <h2>Telefones</h2>
-          <SectionWrapper>
-            {infoUser?.telefone?.map((telefone) => (
-              <PhoneSection
-                key={telefone['phone number']}
-                telefone={telefone}
-              />
-            ))}
-          </SectionWrapper>
-          <h2>Empresas</h2>
-          <SectionWrapper>
-            {infoUser?.empresa?.map((empresa) => (
-              <Company key={empresa['razao social']} empresa={empresa} />
-            ))}
-          </SectionWrapper>
-          <h2>Pessoas</h2>
-          <SectionWrapper>
+          <UserDashboard infoUser={infoUser} />
+          <h2 className="title">Vínculos</h2>
+          <SectionWrapper className="section-wrapper--person">
             {infoUser?.pessoa?.map((pessoa) => (
               <Person key={pessoa['first names']} pessoa={pessoa} />
             ))}
